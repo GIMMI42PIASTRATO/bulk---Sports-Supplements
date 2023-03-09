7b830627fb8be1721c76c5b9b804917ec0eb0393
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-03-10 00:12:02 +0100

    Created header and frist part of the nav bar

---
+++ script.js
@@ -0,0 +1,14 @@
+// class Punto{
+//     constructor(asseX, asseY) {
+//         this.asseX = asseX
+//         this.asseY = asseY
+//     }
+
+//     prova() {
+//         console.log(this.asseX)
+//     }
+// }
+
+// const x1 = new Punto(5, 10)
+
+// x1.prova()
